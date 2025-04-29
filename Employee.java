2d7f07377664f529904555c2bab4b2c150b7c1a9
jpattanashetti <jpattanashetti9@gmail.com>
2025-04-29 09:04:23 +0530

    details

--- Employee.java
+++ Employee.java
@@ -5,21 +5,21 @@ public class Employee {
         String name;
         int id;
         double salary;
-        
-        public Employee(String name,int id,double salary){
+        String location;
+        public Employee(String name,int id,double salary,String location){
             this.name=name;
             this.id=id;
             this.salary=salary;
-           
+            this.location=location;
         }
         public void display(){
             System.out.println("Employee name"+name);
             System.out.println("Employee id"+id);
             System.out.println("Employee salary"+salary);
-            
+            System.out.println("Employee location"+location);
         }
         public static void main(String[] args){
-            Employee emp1=new Employee("Jyothi",101,5000.0);
+            Employee emp1=new Employee("Jyothi",101,5000.0,"Hubli");
             emp1.display();
         }
     }
